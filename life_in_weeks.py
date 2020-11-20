047c63cccd0923e4f804504ebcf10e98f6176e5e
K M H Mubin <kmh.mubin@northsouth.edu>
2020-11-20 07:49:44 +0600

    Your life in weeks calculator Complete

--- life_in_weeks.py
+++ life_in_weeks.py
@@ -7,4 +7,14 @@ age = input("What is your current age?")
 # convert input string to int value
 age_in_int = int(age)
 
+# Calculation for years,months,weeks and days
+years_remaining = 90 - age_in_int
+days_remaining = years_remaining * 365
+weeks_remaining = years_remaining * 52
+months_remaining = years_remaining * 12
 
+# Printing the result
+
+message = f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left"
+
+print(message)
