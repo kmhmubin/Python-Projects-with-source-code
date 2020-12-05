eeaabbe5de39df0b7658e53d992ef00b579d3bce
kmhmubin <kmh.mubin@northsouth.edu>
2020-12-05 18:39:38 +0600

    fix: menu class indentation

--- CoffeeMachineOPP/menu.py
+++ CoffeeMachineOPP/menu.py
@@ -12,27 +12,28 @@ class MenuItem:
             "coffee": coffee
         }
 
-    class Menu:
-        """ Models the menu with drinks"""
 
-        def __init__(self):
-            self.menu = [
-                MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
-                MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5),
-                MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3)
-            ]
+class Menu:
+    """ Models the menu with drinks"""
 
-        def get_items(self):
-            """Returns all the names of the available menu items"""
-            options = ""
-            for item in self.menu:
-                options += f"{item.name}"
-            return options
+    def __init__(self):
+        self.menu = [
+            MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
+            MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5),
+            MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3)
+        ]
 
-        def find_drink(self, order_name):
-            """Searches the menu for a particular drink by name. Returns that item if it exits, otherwise returns
-            None"""
-            for item in self.menu:
-                if item.name == order_name:
-                    return item
-            print("Sorry that item is not available")
+    def get_items(self):
+        """Returns all the names of the available menu items"""
+        options = ""
+        for item in self.menu:
+            options += f"{item.name}"
+        return options
+
+    def find_drink(self, order_name):
+        """Searches the menu for a particular drink by name. Returns that item if it exits, otherwise returns
+        None"""
+        for item in self.menu:
+            if item.name == order_name:
+                return item
+        print("Sorry that item is not available")
