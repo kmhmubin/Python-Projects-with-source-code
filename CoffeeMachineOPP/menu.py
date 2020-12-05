e75eec645241ca6bcb0c1a43df33d073f20b618c
kmhmubin <kmh.mubin@northsouth.edu>
2020-12-05 18:47:18 +0600

    fix: add / to options

--- CoffeeMachineOPP/menu.py
+++ CoffeeMachineOPP/menu.py
@@ -27,7 +27,7 @@ class Menu:
         """Returns all the names of the available menu items"""
         options = ""
         for item in self.menu:
-            options += f"{item.name}"
+            options += f"{item.name} / "
         return options
 
     def find_drink(self, order_name):
