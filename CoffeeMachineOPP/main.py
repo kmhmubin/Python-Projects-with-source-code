7ed729246bffe27140fa923b133656f7fe224938
kmhmubin <kmh.mubin@northsouth.edu>
2020-12-05 18:57:23 +0600

    complete: Coffee Machine in OPP

--- CoffeeMachineOPP/main.py
+++ CoffeeMachineOPP/main.py
@@ -3,3 +3,26 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+# creating object
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+
+# is machine on. default true
+is_on = True
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f" What would you like? ({options}) \n=> ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        is_enough_ingredients = coffee_maker.is_resource_sufficient(drink)
+        is_payment_successful = money_machine.make_payment(drink.cost)
+        if is_enough_ingredients and is_payment_successful:
+            coffee_maker.make_coffee(drink)
