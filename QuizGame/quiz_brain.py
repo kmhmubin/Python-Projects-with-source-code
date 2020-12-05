0ee40f85d66ec9d889f30eca89996e512dc477a6
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-05 23:56:15 +0600

    feat: add quiz class
    In this class, it will generate next question, check answer with data and count score.

---
+++ QuizGame/quiz_brain.py
@@ -0,0 +1,28 @@
+class QuizBrain:
+
+    def __init__(self, ques_list):
+        self.question_number = 0
+        self.score = 0
+        self.question_list = ques_list
+
+    def still_has_question(self):
+        """Return question comparing with total number of question on the list"""
+        return self.question_number < len(self.question_list)
+
+    def next_question(self):
+        """Return the next question and check the answer"""
+        current_question = self.question_list[self.question_number]
+        self.question_number += 1
+        user_answer = input(f"Q. {self.question_number} : {current_question.text} (True / False): ")
+        self.check_answer(user_answer, current_question.answer)
+
+    def check_answer(self, user_answer, correct_answer):
+        """Check the correct answer with user answer."""
+        if user_answer.lower() == correct_answer.lower():
+            self.score += 1
+            print("You got it right !")
+        else:
+            print("That's wrong.")
+        print(f"The correct answer was: {correct_answer}.")
+        print(f"Your current score is: {self.score}/{self.question_number}.")
+        print("\n")
