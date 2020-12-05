a56052d203d1398f6a78dc818080f2ab3b8505ed
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-06 00:04:57 +0600

    complete: quiz game

---
+++ QuizGame/main.py
@@ -0,0 +1,20 @@
+# import classes
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+# empty question bank list
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You've completed the question.")
+print(f"Your final score is: {quiz.score}/ {quiz.question_number}")
