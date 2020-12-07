24b2a678ba5df994bb4150453be057f2252cd462
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-07 16:58:47 +0600

    feat: create GUI screen
    create a new screen with 600x600 size.

---
+++ SnakeGame/main.py
@@ -0,0 +1,16 @@
+from turtle import Turtle, Screen
+
+# creating a object
+screen = Screen()
+
+# setup the screen
+screen.setup(width=600, height=600)
+
+# set background color to black
+screen.bgcolor("black")
+
+# add title of the screen
+screen.title("Snake Game")
+
+# exit on click
+screen.exitonclick()
