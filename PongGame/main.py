5932abf420b16fa0b8176f94c6212367b8b0f891
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-08 00:46:08 +0600

    init: First commit
    add pong game todo list

---
+++ PongGame/main.py
@@ -0,0 +1,11 @@
+# Pong Game
+"""TODO:
+1. create the screen
+2. create and move a paddle
+3. create another paddle
+4. create the ball and make it move
+5. detect collision with wall and bounce
+6. detect collision with paddle
+7. detect when paddle misses
+8. keep score
+"""
