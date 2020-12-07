690f5331eb395a2fe40058c372ed75cb370d66d0
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-07 22:47:46 +0600

    feat: add detection collision with wall
    when snake collied with the wall then show  game over sign.

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -62,5 +62,10 @@ while game_is_on:
         food.refresh()
         scoreboard.increase_score()
 
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
 # exit on click
 screen.exitonclick()
