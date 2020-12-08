f845c5f1c30d48d60a6d73b467f1767c2aa590cd
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-08 14:25:27 +0600

    feat(player): create player and move the player
    now turtle can move with key press and check it weather it reach the finish line or not.

--- TurtleCrossingGame/main.py
+++ TurtleCrossingGame/main.py
@@ -1,15 +1,31 @@
+"""TODO:
+1. Create screen
+2. Create and move cars
+3. Create player
+4. Move the turtle with keypress
+5. Detect collision with car
+6. Detect when turtle reaches the other side
+7. Create a scoreboard
+
+"""
+
 import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+# TODO: Create the game screen
 # creating screen object
 screen = Screen()
 # setup screen size
 screen.setup(width=600, height=600)
 # turn off turtle animation
 screen.tracer(0)
+
+# TODO: Create player
+# creating player object
+player = Player()
 
 # game on or not
 is_game_on = True
--- TurtleCrossingGame/player.py
+++ TurtleCrossingGame/player.py
@@ -1,3 +1,6 @@
+# TODO: Create player and Move the turtle with keypress
+from turtle import Turtle
+
 # constant for players
 STARTING_POSITION = (0, -280)
 MOVE_DISTANCE = 10
@@ -5,5 +8,26 @@ FINISH_LINE_Y = 280
 
 
 # player class
-class Player:
-    pass
+class Player(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.go_to_start()
+        self.setheading(90)
+
+    def go_up(self):
+        """Move the turtle forward """
+        self.forward(MOVE_DISTANCE)
+
+    def go_to_start(self):
+        """Set starting position for turtle"""
+        self.goto(STARTING_POSITION)
+
+    def is_at_finish_line(self):
+        """Return boolean if turtle reach the finish line"""
+        if self.ycor() > FINISH_LINE_Y:
+            return True
+        else:
+            return False
