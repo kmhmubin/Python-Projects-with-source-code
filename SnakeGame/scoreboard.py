ac55dafd5c5836a80d474d7d837e2e7473599cd3
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-08 19:40:22 +0600

    feat: store high score date

--- SnakeGame/scoreboard.py
+++ SnakeGame/scoreboard.py
@@ -13,7 +13,8 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -29,7 +30,10 @@ class Scoreboard(Turtle):
         """if collision then game over."""
         if self.score > self.high_score:
             self.high_score = self.score
-            # it will reset to zero for new game
+            # store high score in the data.txt file
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+                # it will reset to zero for new game
             self.score = 0
             self.update_scoreboard()
 
