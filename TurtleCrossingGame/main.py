09978050a79102fa8396cd22721f8d20da1c1fcf
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-08 14:04:16 +0600

    feat: create a screen

---
+++ TurtleCrossingGame/main.py
@@ -0,0 +1,20 @@
+import time
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoarboard import Scoreboard
+
+# creating screen object
+screen = Screen()
+# setup screen size
+screen.setup(width=600, height=600)
+# turn off turtle animation
+screen.tracer(0)
+
+# game on or not
+is_game_on = True
+
+while is_game_on:
+    time.sleep(0.1)
+    # update the screen
+    screen.update()
