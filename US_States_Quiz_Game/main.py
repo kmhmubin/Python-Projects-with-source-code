f573e36e56a1738e8cbd0195d1032caeef67d438
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-09 17:30:02 +0600

    feat: add popup answer input box

--- US_States_Quiz_Game/main.py
+++ US_States_Quiz_Game/main.py
@@ -1,4 +1,18 @@
+"""TODO:
+1. create a screen
+2. show blank US state image
+3. Take user input
+4. Convert the input to Title case
+5. check if the guess is amoung the 50 states
+6. write correct guess onto the map
+7. allow the user to keep guessing
+8. record the correct guesses
+9 keep track of the score
+"""
+
+
 import turtle
+import pandas
 
 # creating screen object
 screen = turtle.Screen()
@@ -11,14 +25,17 @@ screen.addshape(image)
 # changing turtle shape
 turtle.shape(image)
 
+# # get the coordinates from mouse click
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+#
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
 
-# get the coordinates from mouse click
-def get_mouse_click_coor(x, y):
-    print(x, y)
-
-
-turtle.onscreenclick(get_mouse_click_coor)
-turtle.mainloop()
+# popup input option
+answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+print(answer_state)
 
 # screen exit on click
 # screen.exitonclick()
