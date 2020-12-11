05bff86503bac7cd2823bf77bcf31a06fc809377
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-11 19:56:29 +0600

    feat: UI Design
    create a beautiful ui design for pomodoro program

--- Pomodoro/main.py
+++ Pomodoro/main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -16,3 +17,22 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+# creating window
+window = Tk()
+# window title
+window.title("Pomodoro")
+# add padding to the window and add background color
+window.config(padx=100, pady=50, bg=YELLOW)
+# creating a canvas, add background color and remove border around image
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+# assign the image location to a variable
+tomato_img = PhotoImage(file="tomato.png")
+# add image in the canvas in the center by "/" half of the dimension
+canvas.create_image(100, 112, image=tomato_img)
+# add text on the top of tomato photo and customize text values
+canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+# show the canvas on the window
+canvas.pack()
+
+# window show up
+window.mainloop()
