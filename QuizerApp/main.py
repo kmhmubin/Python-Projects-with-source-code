786b54a349f536c90619c8ffc8a8811f4cc21ad4
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-16 21:37:22 +0600

    feat: main quiz app driver the main script for run the app

--- QuizerApp/main.py
+++ QuizerApp/main.py
@@ -1,2 +1,23 @@
 # QUIZ GUI APP USING TRIVIA API
 
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+# assign empty list for question
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+# getting question
+quiz = QuizBrain(question_bank)
+
+# is quiz running
+while quiz.still_has_question():
+    quiz.next_question()
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
