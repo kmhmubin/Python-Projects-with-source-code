301fbab6a61cb9080eb298ad9e7e775cd575191a
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-16 19:08:15 +0600

    feat: checking is the night time and position of iss
    check the current time with sunrise or sunset time and check the position of iss based on my latitude and longitude

--- ISS-Tracker/main.py
+++ ISS-Tracker/main.py
@@ -1,1 +1,44 @@
 # international Space Station Tracker
+import requests
+from datetime import datetime
+
+# Constant values of my latitudes and Longitude
+MY_LAT = 23.870013
+MY_LONG = 90.408708
+
+
+def is_iss_overhead():
+    # fetching the response from api
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    # rise error if failed
+    response.raise_for_status()
+    # fetching data from api requests
+    data = response.json()
+
+    # catching iss latitude
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+
+    # my position is within +5 or -5 degrees of the iss position
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5:
+        return True
+
+
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "long": MY_LONG,
+        "formatted": 0,
+    }
+
+    # fetching data from sunrise and sunset api
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    sun_data = response.json()
+    sunrise = int(sun_data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(sun_data["results"]["sunset"].split("T")[1].split(":")[0])
+    # current time in hour
+    time_now = datetime.now().hour
+    # check the time
+    if time_now >= sunset or time_now <= sunrise:
+        return True
