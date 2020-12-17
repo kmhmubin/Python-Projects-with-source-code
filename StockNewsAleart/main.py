c388c98dae054c7be8dc5e854425d1306db3b495
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-17 20:12:02 +0600

    feat: generate 3 news articles based on company name

--- StockNewsAleart/main.py
+++ StockNewsAleart/main.py
@@ -9,6 +9,7 @@ NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
 
 # API KEYS
 STOCK_API_KEY = "EYYHCJH0RGV5H0C0"
+NEWS_API_KEY = "f513ec614dd7446cbc7339cbba57e9a7"
 
 ## STEP 1: Use https://www.alphavantage.co/documentation/#daily
 # When stock price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
@@ -42,7 +43,6 @@ print(difference)
 difference_percent = (difference / float(yesterday_closing_price)) * 100
 print(difference_percent)
 # TODO 5. - If TODO4 percentage is greater than 5 then print("Get News").
-
 ## STEP 2: https://newsapi.org/
 # Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
@@ -51,21 +51,30 @@ print(difference_percent)
 # TODO 7. - Use Python slice operator to create a list that contains the first 3 articles. Hint:
 #  https://stackoverflow.com/questions/509211/understanding-slice-notation
 
-
 ## STEP 3: Use twilio.com/docs/sms/quickstart/python
 # to send a separate message with each article's title and description to your phone number.
+if difference_percent > 1:
+    # get the news
+    news_parameters = {
+        "apikey": NEWS_API_KEY,
+        "qInTitle": COMPANY_NAME,
+    }
 
-# TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
+    news_response = requests.get(NEWS_ENDPOINT, params=news_parameters)
+    articles = news_response.json()["articles"]
+    three_article = articles[:3]
+    print(three_article)
 
-# TODO 9. - Send each article as a separate message via Twilio.
+    # TODO 8. - Create a new list of the first 3 article's headline and description using list comprehension.
 
+    # TODO 9. - Send each article as a separate message via Twilio.
 
-# Optional TODO: Format the message like this:
-"""TSLA: 🔺2% Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. Brief: We at Insider Monkey have 
-gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings 
-show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash. 
-or 
-"TSLA: 🔻5% Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. Brief: We at Insider Monkey 
-have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F 
-filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus 
-market crash. """
+    # Optional TODO: Format the message like this:
+    """TSLA: 🔺2% Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. Brief: We at Insider Monkey have 
+    gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings 
+    show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash. 
+    or 
+    "TSLA: 🔻5% Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. Brief: We at Insider Monkey 
+    have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F 
+    filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus 
+    market crash. """
