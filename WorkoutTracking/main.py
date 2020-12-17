5df2f3f093db5a18ca30437815260bf1945ed151
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-18 04:21:38 +0600

    feat: add data to your google excel sheet

--- WorkoutTracking/main.py
+++ WorkoutTracking/main.py
@@ -1,12 +1,20 @@
 import os
 import requests
+from datetime import datetime
 
 # Constant for apis
 
-APP_ID = os.getenv("EXERCISE_ID")
-API_KEY = os.getenv("EXERCISE_API_KEY")
+APP_ID = os.environ["NT_APP_ID"]
+API_KEY = os.environ["NT_APP_KEY"]
 
-NUTRITIONIX_ENDPOINT = " https://trackapi.nutritionix.com/v2/natural/exercise"
+NUTRITIONIX_ENDPOINT = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+# Sheety api endpoint
+SHEETY_ENDPOINT = os.environ["SHEETY_ENDPOINT"]
+
+# sheet authentication
+USERNAME = os.environ["USERNAME"]
+PASSWORD = os.environ["PASSWORD"]
 
 # constant for user
 GENDER = "male"
@@ -14,10 +22,10 @@ WEIGHT_KG = 78.1
 HEIGHT_CM = 176.53
 AGE = 23
 
-# exercise input
+# exercise input 
 exercise_input = input("Tell me which exercise you did: ")
 
-# api header
+# exercise api header
 headers = {
     "x-app-id": APP_ID,
     "x-app-key": API_KEY,
@@ -34,4 +42,25 @@ exercise_parameters = {
 # exercise response
 response = requests.post(url=NUTRITIONIX_ENDPOINT, json=exercise_parameters, headers=headers)
 result = response.json()
-print(result)
+# print(result)
+
+# ----------- Excel sheet ----------- # 
+# today date and current time
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+# add those values into the excel sheet
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+
+    sheet_response = requests.post(url=SHEETY_ENDPOINT, json=sheet_inputs, auth=(USERNAME, PASSWORD))
+
+    print(sheet_response.text)
