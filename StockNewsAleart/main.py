03b7503d6a2c10fd09a6ccdda09ae3c9234d0d81
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-17 19:49:03 +0600

    feat: find the positive difference of 2 days closing price

--- StockNewsAleart/main.py
+++ StockNewsAleart/main.py
@@ -35,7 +35,8 @@ day_before_yesterday_closing_price = day_before_yesterday_data["4. close"]
 print(day_before_yesterday_closing_price)
 # TODO 3. - Find the positive difference between 1 and 2. e.g. 40 - 20 = -20, but the positive difference is 20.
 #  Hint: https://www.w3schools.com/python/ref_func_abs.asp
-
+difference = abs(float(yesterday_closing_price) - float(day_before_yesterday_closing_price))
+print(difference)
 # TODO 4. - Work out the percentage difference in price between closing price yesterday and closing price the day
 #  before yesterday.
 
