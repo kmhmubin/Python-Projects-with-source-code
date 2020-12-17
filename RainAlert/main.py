baa6aca0eac641ec2dc777676b103882a25f484b
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-17 17:38:20 +0600

    feat: add twilio api for sms notification.

--- RainAlert/main.py
+++ RainAlert/main.py
@@ -1,14 +1,22 @@
 # Rain Alert Using twilio
-
+import os
 import requests
+from twilio.rest import Client
 
 API_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall"
+# get the api key from environment variable
+API_KEY = os.getenv("OWM_API_KEY")
 
-API_KEY = "f32840ec9669473b25c131c30a91e3e3"
+PHONE_NUMBER = "+12516640218"
+
+# Your Account SID from twilio.com/console
+account_sid = os.getenv("ACCOUNT_SID")
+# Your Auth Token from twilio.com/console
+auth_token = os.getenv("AUTH_TOKEN")
 
 weather_parameters = {
-    "lat": 23.870038,
-    "lon": 90.408723,
+    "lat": 1.352083,
+    "lon": 103.819839,
     "appid": API_KEY,
     "exclude": "current,minutely,daily",
 }
@@ -29,4 +37,11 @@ for hour_data in weather_slice:
         will_rain = True
 
 if will_rain:
-    print("Bring an umbrella")
+    client = Client(account_sid, auth_token)
+    message = client.messages \
+        .create(
+        body="It's Going to rain today. Remember to bring an Umbrella ",
+        from_=PHONE_NUMBER,
+        to='+8801713343232'
+    )
+    print(message.status)
