71f1bd5408003fda9a1c2bf146b53c61a741963a
K M H Mubin <kmh.mubin@northsouth.edu>
2020-12-30 18:31:45 +0600

    feat: add login method for login in instagram

--- InstaFollowerBot/main.py
+++ InstaFollowerBot/main.py
@@ -1,4 +1,6 @@
+import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 CHROME_DRIVER_PATH = "C:\chromedriver\chromedriver.exe"
 SIMILAR_ACCOUNT = "akifahi"
@@ -12,7 +14,19 @@ class InstaFollower:
         self.driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
 
     def login(self):
-        pass
+        self.driver.get("https://www.instagram.com/accounts/login/")
+        time.sleep(5)
+
+        username = self.driver.find_element_by_name("username")
+        password = self.driver.find_element_by_name("password")
+
+        # send the keys
+        username.send_keys(USERNAME)
+        time.sleep(1)
+        password.send_keys(PASSWORD)
+        time.sleep(2)
+
+        password.send_keys(Keys.ENTER)
 
     def find_follower(self):
         pass
