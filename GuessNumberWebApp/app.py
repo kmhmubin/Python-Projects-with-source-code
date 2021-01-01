c93e605952b213b8b5556dbc54aa70fd072e8510
K M H Mubin <kmh.mubin@northsouth.edu>
2021-01-01 17:49:16 +0600

    feat: guess the number and show the result

--- GuessNumberWebApp/app.py
+++ GuessNumberWebApp/app.py
@@ -1,12 +1,32 @@
 from flask import Flask
+import random
 
+# random number 0 to 9
+random_number = random.randint(0, 9)
+print(random_number)
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def heme():
+    return "<h1>Guess a number between 0 and 9</h1>" \
+           "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'/>"
+
+
+# enter guess number in URL
+@app.route("/<int:guess>")
+def guess_number(guess):
+    if guess > random_number:
+        return "<h1 style='color: purple'>Too high, try again!</h1>" \
+               "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'/>"
+
+    elif guess < random_number:
+        return "<h1 style='color: red'>Too low, try again!</h1>" \
+               "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
+    else:
+        return "<h1 style='color: green'>You found me!</h1>" \
+               "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
