7e590bac559f38e480afe74d7411e24bd25e37dd
K M H Mubin <kmh.mubin@northsouth.edu>
2021-01-03 19:22:18 +0600

    feat: update header in jinja template
    update all the static files links with jinja2 template

--- FlasKBlog/app.py
+++ FlasKBlog/app.py
@@ -9,5 +9,10 @@ def home():
     return render_template("index.html")
 
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
 if __name__ == '__main__':
     app.run()
