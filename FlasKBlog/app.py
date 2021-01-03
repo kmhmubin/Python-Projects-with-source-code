87c7290155c67491eaa1d65a0b73e980237e5e8a
K M H Mubin <kmh.mubin@northsouth.edu>
2021-01-03 20:42:04 +0600

    feat: get posts and show on website
    grab all the posts from api and show all posts titles, subtitles, author name and publish date.

--- FlasKBlog/app.py
+++ FlasKBlog/app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
 import requests
 
+# fake post from apis
+posts = requests.get("https://api.npoint.io/493622f5077bee2314fa").json()
+
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html", all_post=posts)
+
+
+@app.route('/post/<int:index>')
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 
 @app.route('/about')
