fdd8b28179d5bccb77b2f194f99ace6b5c8d7ceb
K M H Mubin <kmh.mubin@northsouth.edu>
2021-01-03 19:49:40 +0600

    fix: hero images url for index and about pages

--- FlasKBlog/app.py
+++ FlasKBlog/app.py
@@ -14,5 +14,10 @@ def about():
     return render_template("about.html")
 
 
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+
+
 if __name__ == '__main__':
     app.run()
