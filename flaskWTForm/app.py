61d14cbe8c74649eacfe87f7364d2fa68f39fff1
K M H Mubin <kmh.mubin@northsouth.edu>
2021-01-04 21:24:27 +0600

    feat: add csrf protection to the form

---
+++ flaskWTForm/app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+
+
+# creating a form
+
+class LoginForm(FlaskForm):
+    email = StringField('Email')
+    password = PasswordField('Password')
+
+
+app = Flask(__name__)
+# app secret key
+app.secret_key = b'1\xadL~]\x8e2\xe4\xf6\xa6\xe0\xefWL\xb1\x92'
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+
+@app.route('/login')
+def login():
+    login_form = LoginForm()
+    return render_template('login.html', form=login_form)
+
+
+if __name__ == '__main__':
+    app.run()
