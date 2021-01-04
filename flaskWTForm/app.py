e4029ca823c96f5d723331e428a302869643ddd4
K M H Mubin <kmh.mubin@northsouth.edu>
2021-01-04 21:57:51 +0600

    feat: add validation to the form

--- flaskWTForm/app.py
+++ flaskWTForm/app.py
@@ -1,13 +1,14 @@
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 
 # creating a form
 
 class LoginForm(FlaskForm):
-    email = StringField(label='Email')
-    password = PasswordField(label='Password')
+    email = StringField(label='Email', validators=[DataRequired()])
+    password = PasswordField(label='Password', validators=[DataRequired()])
     submit = SubmitField(label="Login")
 
 
@@ -21,9 +22,14 @@ def home():
     return render_template("index.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=["GET", "POST"])
 def login():
     login_form = LoginForm()
+    if login_form.validate_on_submit():
+        if login_form.email.data == "admin@email.com" and login_form.password.data == "123456":
+            return render_template("success.html")
+        else:
+            return render_template("denied.html")
     return render_template('login.html', form=login_form)
 
 
