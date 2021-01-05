2928ae52273ab7808cd980da5658778d76f77aa3
K M H Mubin <kmh.mubin@northsouth.edu>
2021-01-06 01:56:05 +0600

    init: basic project structure

---
+++ BookTrackerApp/app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+# all books list
+
+all_books = []
+
+
+@app.route('/')
+def home():
+    return 'Hello World!'
+
+
+@app.route('/add')
+def add():
+    pass
+
+
+if __name__ == '__main__':
+    app.run()
