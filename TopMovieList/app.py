7829d2b64b8de3cb04dce1a0dc44b7fae64d2b4c
K M H Mubin <kmh.mubin@northsouth.edu>
2021-01-06 20:04:08 +0600

    feat: Create a movie DB

--- TopMovieList/app.py
+++ TopMovieList/app.py
@@ -10,6 +10,26 @@ app = Flask(__name__)
 # secret key
 app.secret_key = b'\xb9\xd6\xc7\xc4\xb5\xf0\xc4y\xc6\xb0\r\xc3`!\xca~'
 
+# creating db
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# creating table
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+db.create_all()
+
 
 @app.route('/')
 def home():
