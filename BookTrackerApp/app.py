af367de25cda527ffa53137c111f85aa0a0a3f22
K M H Mubin <kmh.mubin@northsouth.edu>
2021-01-06 13:32:41 +0600

    feat: save record into DB from form input

--- BookTrackerApp/app.py
+++ BookTrackerApp/app.py
@@ -5,13 +5,10 @@ app = Flask(__name__)
 
 app.secret_key = b'\xeb\x8b!\x9c\x8cF}\xa0\xa3:\xe4f\xe9\xc7\xc4\x9b'
 
-# all books list
-
-all_books = []
-
 # creating database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-books-collection.db'
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books-collection.db'
+# silence the deprecation warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # connecting the database with flask app
 db = SQLAlchemy(app)
 
@@ -23,38 +20,29 @@ class Book(db.Model):
     author = db.Column(db.String(250), nullable=False)
     rating = db.Column(db.Float, nullable=False)
 
-    # this allow each book object to be identify by its title
-    def __repr__(self):
-        return f'<Book {self.title}>'
-
 
 # create the database
 db.create_all()
 
-# create the record in db
-new_book = Book(id=1, title="Harry Potter", author="J. K. Rowling", rating=9.3)
-
-db.session.add(new_book)
-
-db.session.commit()
-
 
 @app.route('/')
 def home():
-    return render_template('index.html', books=all_books)
+    return render_template('index.html')
 
 
 @app.route('/add', methods=["GET", "POST"])
 def add():
     if request.method == "POST":
-        new_book = {
-            "title": request.form["title"].title(),
-            "author": request.form["author"].title(),
-            "rating": request.form["rating"].title()
-        }
-        # save in the empty list
-        all_books.append(new_book)
-        # print(all_books)
+        # create records
+        new_book = Book(
+            title=request.form["title"],
+            author=request.form["author"],
+            rating=request.form["rating"]
+        )
+        # save record into db
+        db.session.add(new_book)
+        # commit the db
+        db.session.commit()
         # after submission success then return home page
         return redirect(url_for('home'))
 
