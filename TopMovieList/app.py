48f22c5c43650048d89bbe85dd31a8e25386c639
K M H Mubin <kmh.mubin@northsouth.edu>
2021-01-06 22:37:40 +0600

    feat: add frontend design

--- TopMovieList/app.py
+++ TopMovieList/app.py
@@ -30,26 +30,29 @@ class Movie(db.Model):
 
 db.create_all()
 
+
 # Add movie data manually for checking
-
-new_movie = Movie(
-    title="Phone Booth",
-    year=2002,
-    description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's "
-                "sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-    rating=7.3,
-    ranking=10,
-    review="My favourite character was the caller.",
-    img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-)
-
-db.session.add(new_movie)
-db.session.commit()
+#
+# new_movie = Movie(
+#     title="Phone Booth",
+#     year=2002,
+#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's "
+#                 "sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#     rating=7.3,
+#     ranking=10,
+#     review="My favourite character was the caller.",
+#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+# )
+#
+# db.session.add(new_movie)
+# db.session.commit()
 
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    # get all the movies from DB
+    all_movies = Movie.query.all()
+    return render_template('index.html', movies=all_movies)
 
 
 if __name__ == '__main__':
