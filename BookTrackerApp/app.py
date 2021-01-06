ad6c88bf6f15f370b457d283d25cd3a42796b13f
K M H Mubin <kmh.mubin@northsouth.edu>
2021-01-06 12:18:33 +0600

    feat: add book data and show in home page
    Book information are added and it will show in the homepage

--- BookTrackerApp/app.py
+++ BookTrackerApp/app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+app.secret_key = b'\xeb\x8b!\x9c\x8cF}\xa0\xa3:\xe4f\xe9\xc7\xc4\x9b'
+
 # all books list
 
 all_books = []
@@ -9,12 +11,24 @@ all_books = []
 
 @app.route('/')
 def home():
-    return 'Hello World!'
+    return render_template('index.html', books=all_books)
 
 
-@app.route('/add')
+@app.route('/add', methods=["GET", "POST"])
 def add():
-    pass
+    if request.method == "POST":
+        new_book = {
+            "title": request.form["title"],
+            "author": request.form["author"],
+            "rating": request.form["rating"]
+        }
+        # save in the empty list
+        all_books.append(new_book)
+        # print(all_books)
+        # after submission success then return home page
+        return redirect(url_for('home'))
+
+    return render_template('add.html')
 
 
 if __name__ == '__main__':
