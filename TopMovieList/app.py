558e49421454fd91187bcf5009bd3d23d2ce7afa
K M H Mubin <kmh.mubin@northsouth.edu>
2021-01-07 17:04:48 +0600

    fix: displaying movie card

--- TopMovieList/app.py
+++ TopMovieList/app.py
@@ -8,7 +8,7 @@ import requests
 
 # CONSTANTS
 
-MOVIE_DB_API_KEY = "YOUR API KEY"
+MOVIE_DB_API_KEY = "Your Own API key"
 MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
 MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
 MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
@@ -26,7 +26,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-# creating table
+##CREATE TABLE
 class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(250), unique=True, nullable=False)
@@ -41,7 +41,10 @@ class Movie(db.Model):
 db.create_all()
 
 
-# edit the rating and reviews
+class FindMovieForm(FlaskForm):
+    title = StringField("Movie Title", validators=[DataRequired()])
+    submit = SubmitField("Add Movie")
+
 
 class RateMovieForm(FlaskForm):
     rating = StringField("Your Rating Out of 10 e.g. 7.5")
@@ -49,56 +52,65 @@ class RateMovieForm(FlaskForm):
     submit = SubmitField("Done")
 
 
-# find movies from The Movie Database
-class FindMovieForm(FlaskForm):
-    title = StringField('Movie Title', validators=[DataRequired()])
-    submit = SubmitField("Add Movie")
-
-
-@app.route('/')
+@app.route("/")
 def home():
-    # get all the movies from DB
-    all_movies = Movie.query.all()
-    return render_template('index.html', movies=all_movies)
+    all_movies = Movie.query.order_by(Movie.rating).all()
+    for i in range(len(all_movies)):
+        all_movies[i].ranking = len(all_movies) - i
+    db.session.commit()
+    return render_template("index.html", movies=all_movies)
 
 
-@app.route('/edit', methods=["GET", "POST"])
+@app.route("/add", methods=["GET", "POST"])
+def add_movie():
+    form = FindMovieForm()
+    if form.validate_on_submit():
+        movie_title = form.title.data
+
+        response = requests.get(MOVIE_DB_SEARCH_URL, params={"api_key": MOVIE_DB_API_KEY, "query": movie_title})
+        data = response.json()["results"]
+        return render_template("select.html", options=data)
+    return render_template("add.html", form=form)
+
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    if movie_api_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+        response = requests.get(movie_api_url, params={"api_key": MOVIE_DB_API_KEY, "language": "en-US"})
+        data = response.json()
+        new_movie = Movie(
+            title=data["title"],
+            year=data["release_date"].split("-")[0],
+            img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+            description=data["overview"]
+        )
+        db.session.add(new_movie)
+        db.session.commit()
+        return redirect(url_for("rate_movie", id=new_movie.id))
+
+
+@app.route("/edit", methods=["GET", "POST"])
 def rate_movie():
-    # inherit form the class
     form = RateMovieForm()
-    # get the id
-    movie_id = request.args.get('id')
-    # check with db
+    movie_id = request.args.get("id")
     movie = Movie.query.get(movie_id)
-    # if the id match then update
     if form.validate_on_submit():
         movie.rating = float(form.rating.data)
         movie.review = form.review.data
         db.session.commit()
         return redirect(url_for('home'))
-    return render_template('edit.html', movie=movie, form=form)
+    return render_template("edit.html", movie=movie, form=form)
 
 
-@app.route('/delete')
+@app.route("/delete")
 def delete_movie():
     movie_id = request.args.get("id")
     movie = Movie.query.get(movie_id)
     db.session.delete(movie)
     db.session.commit()
-    return redirect(url_for('home'))
-
-
-@app.route('/add', methods=["GET", "POST"])
-def add_movie():
-    form = FindMovieForm()
-
-    # get information from The Movie database
-    if form.validate_on_submit():
-        movie_title = form.title.data
-        response = requests.get(MOVIE_DB_SEARCH_URL, params={"api_key": MOVIE_DB_API_KEY, "query": movie_title})
-        data = response.json()["results"]
-        return render_template("select.html", options=data)
-    return render_template("add.html", form=form)
+    return redirect(url_for("home"))
 
 
 if __name__ == '__main__':
